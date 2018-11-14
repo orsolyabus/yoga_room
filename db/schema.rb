d4eecf8d3cb6ff1b1e83085dcae944dccb0e265b
Orsolya Torokne Bus <orsolyabus@gmail.com>
2018-11-13 22:46:55 -0800

    remove duration column from yoga classes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_021605) do
+ActiveRecord::Schema.define(version: 2018_11_14_064454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2018_11_11_021605) do
   create_table "yoga_classes", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
