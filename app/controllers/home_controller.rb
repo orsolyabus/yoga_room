f2187cbd53f01054799645b988f5ce31520221cf
Orsolya Torokne Bus <orsolyabus@gmail.com>
2019-02-05 14:10:23 -0800

    add contact page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,8 @@ class HomeController < ApplicationController
   def index
   end
 
-  def search
-    @params = params
-    @search_params = helpers.get_search_params(params)
-    @search_result = helpers.search(@search_params)
+  def contact
+    @issue = Issue.new
   end
 
   private
