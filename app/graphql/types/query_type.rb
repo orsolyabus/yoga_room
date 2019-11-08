3d75ce8804a8ac7174eb2406517ca9c2284575f9
Orsolya Torokne Bus <orsolyabus@gmail.com>
2019-11-08 14:37:21 -0800

    add user queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,8 +1,31 @@
 module Types
   class QueryType < BaseObject
-    field :all_users, [UserType], null: false
+    field :all_users, [UserType], null: false do
+      description 'Find all users'
+    end
+    field :all_teachers, [UserType], null: false do
+      description 'Find all teachers'
+    end
+    field :all_students, [UserType], null: false do
+      description 'Find all students'
+    end
+    field :user, UserType, null: false do
+      description 'Find a user by id'
+      argument :id, ID, required: true
+    end
+    
     def all_users
       User.all
     end
+    def all_teachers
+      User.where(is_teacher: true)
+    end
+    def all_students
+      User.where(is_teacher: false)
+    end
+    def user(id:)
+      User.find(id)
+    end
+    
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,7 +3,7 @@ module Types
     field :id, ID, null: false
     field :name, String, null:false
     field :email, String, null: false
-    # field :is_teacher, Boolean, null: false
-    # field :last_login, DateTime
+    field :is_teacher, Boolean, null: false
+    field :password, String, null: false
   end
 end
