24256f263dd3bec99e9fd2b32cff27277043e8bb
Orsolya Torokne Bus <orsolyabus@gmail.com>
2019-11-08 14:46:56 -0800

    add create user mutation

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -3,16 +3,18 @@ module Mutations
     # arguments passed to the `resolved` method
     argument :name, String, required: true
     argument :email, String, required: true
-    # argument :is_teacher, Boolean, required: true
+    argument :is_teacher, Boolean, required: true
+    argument :password, String, required: true
 
     # return type from the mutation
     type Types::UserType
 
-    def resolve(name: nil, email: nil)
+    def resolve(name: nil, email: nil, is_teacher: false, password: nil)
       User.create!(
         name: name,
         email: email,
-        # is_teacher: is_teacher,
+        is_teacher: is_teacher,
+        password: password,
       )
     end
   end
