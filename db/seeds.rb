e4cd3f504380025fcac36a11d792298a6642ebac
Orsolya Torokne Bus <orsolyabus@gmail.com>
2019-11-18 12:32:21 -0800

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,7 @@ Location.delete_all
 Profile.delete_all
 
 User.delete_all
+AdminUser.delete_all if Rails.env.development?
 
 PASSWORD = "secret"
 
@@ -28,31 +29,30 @@ super_user = User.create(
 10.times do
   u = FactoryBot.create(:user)
   profile = FactoryBot.create(:profile, user: u)
+  n = [0,1,2,3,4].sample
+  n.times do
+    FactoryBot.create(:location, profile: profile)
+  end
 end
-puts "users and profiles seeded"
+puts "users and profiles and locations seeded"
 
 teachers = Profile.all
 
-# location
-10.times do 
-  l = FactoryBot.create(:location)
-end
-
-locations = Location.all
-puts "locations seeded"
 # yoga class + schedule
-30.times do
-  yc = FactoryBot.build(:yoga_class, profile: teachers.sample)
-  yc.location = locations.sample
+40.times do
+  t = teachers.sample
+  yc = FactoryBot.build(:yoga_class, profile: t)
+  yc.location = t.locations.sample
   yc.save
   s = FactoryBot.create(:schedule, yoga_class: yc)
 end
 puts "yoga classes seeded"
+
 # saved search
 30.times do
   s = FactoryBot.create(:saved_search, user: User.all.sample)
 end
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: PASSWORD, password_confirmation: PASSWORD) if Rails.env.development?
 
 puts "seeding done"
